fde482a850e2224084ca21b599bc99f217195be3
Philippe Charrière <ph.charriere@gmail.com>
2023-11-08 16:36:16 +0000

    💾 Saved.

--- Hello/src/main/java/garden/bots/Main.java
+++ Hello/src/main/java/garden/bots/Main.java
@@ -6,7 +6,6 @@ import java.io.File;
 import com.dylibso.chicory.runtime.Module;
 import com.dylibso.chicory.runtime.Instance;
 
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -21,3 +20,14 @@ public class Main {
 
 // mvn package
 // java -jar target/Hello-1.0-SNAPSHOT.jar 
+// mvn exec:java -Dexec.mainClass="garden.bots.Main"
+
+/*
+mvn install:install-file \
+   -Dfile=/workspace/arhuaco/Hello/src/main/resources/runtime-1.0-SNAPSHOT.jar \
+   -DgroupId=com.dylibso.chicory \
+   -DartifactId=chicory \
+   -Dversion=0.0.1 \
+   -Dpackaging=jar \
+   -DgeneratePom=true
+*/
