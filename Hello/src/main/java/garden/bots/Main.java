58f6cb596da4bf30a2487346ecf1ce6b570b632f
Philippe Charrière <ph.charriere@gmail.com>
2023-11-08 15:50:30 +0000

    💾 Saved.

---
+++ Hello/src/main/java/garden/bots/Main.java
@@ -0,0 +1,22 @@
+package garden.bots;
+
+
+import java.io.File;
+
+import com.dylibso.chicory.runtime.Module;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        Module module = Module.build(new File("/workspace/arhuaco/factorial.wasm"));
+        
+        Instance instance = module.instantiate();
+
+    }
+}
+
+// mvn package
+// java -jar target/Hello-1.0-SNAPSHOT.jar 
